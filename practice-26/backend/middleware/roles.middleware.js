575cb2ea7f3e9fc29a6d02399ac68a129d565877
Ilya Konst <mendellq@gmail.com>
2026-05-12 23:03:35 +0300

    Add practical28 branch

--- practice-26/backend/middleware/roles.middleware.js
+++
@@ -1,9 +0,0 @@
-function rolesMiddleware(allowedRoles) {
-  return (req, res, next) => {
-    if (!req.user || !allowedRoles.includes(req.user.role))
-      return res.status(403).json({ error: "Forbidden" });
-    next();
-  };
-}
-
-module.exports = { rolesMiddleware };
